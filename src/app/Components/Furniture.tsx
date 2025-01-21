b1c182a45a7b2952217cc1ea75890b4303336de5
Muhammad Ibrahim <160228904+Ibrahimnazee@users.noreply.github.com>
2025-01-21 19:16:36 +0500

    Update Furniture.tsx

--- src/app/Components/Furniture.tsx
+++ src/app/Components/Furniture.tsx
@@ -1,4 +1,80 @@
-import Image from "next/image";
+import Image from 'next/image'
+
+const galleryImages = [
+    {
+      id: '1',
+      src: '/images/left-cutted.png',
+      alt: 'Modern shelf setup with plants',
+      width: 280,
+      height: 500,
+      className: 'lg:block hidden col-span-1 lg:col-start-1 lg:col-end-3 lg:row-start-1 lg:row-end-3'
+    },
+    {
+      id: '2',
+      src: '/images/laptop.png',
+      alt: 'Minimalist workspace setup',
+      width: 450,
+      height: 300,
+      className: 'col-span-1 lg:col-start-3 lg:col-end-6 lg:row-start-1 lg:row-end-2'
+    },
+    {
+      id: '3',
+      src: '/images/chair.png',
+      alt: 'Vintage brown chair',
+      width: 280,
+      height: 400,
+      className: 'col-span-1 lg:col-start-3 lg:col-end-5 lg:row-start-2 lg:row-end-3'
+    },
+    {
+      id: '4',
+      src: '/images/showpiece.png',
+      alt: 'Small table with vase',
+      width: 280,
+      height: 280,
+      className: 'col-span-1 lg:col-start-5 lg:col-end-6 lg:row-start-2 lg:row-end-3'
+    },
+    {
+      id: '5',
+      src: '/images/table1.png',
+      alt: 'Modern dining room',
+      width: 400,
+      height: 600,
+      className: 'col-span-1 lg:col-start-6 lg:col-end-9 lg:row-start-1 lg:row-end-4'
+    },
+    {
+      id: '6',
+      src: '/images/bed.png',
+      alt: 'Contemporary bedroom setup',
+      width: 450,
+      height: 350,
+      className: 'col-span-1 lg:col-start-9 lg:col-end-12 lg:row-start-1 lg:row-end-2'
+    },
+    {
+      id: '7',
+      src: '/images/kitchen.png',
+      alt: 'Modern kitchen counter',
+      width: 280,
+      height: 280,
+      className: 'col-span-1 lg:col-start-12 lg:col-end-14 lg:row-start-1 lg:row-end-2'
+    },
+    {
+      id: '8',
+      src: '/images/artwall.png',
+      alt: 'Decorative wall art',
+      width: 280,
+      height: 280,
+      className: 'col-span-1 lg:col-start-9 lg:col-end-11 lg:row-start-2 lg:row-end-3'
+    },
+    {
+      id: '9',
+      src: '/images/table2.png',
+      alt: 'Wall mounted shelf',
+      width: 280,
+      height: 280,
+      className: 'col-span-1 lg:col-start-12 lg:col-end-14 lg:row-start-2 lg:row-end-3'
+    }
+  ];
+  
 
 export default function Furniture() {
   return (
@@ -12,98 +88,47 @@ export default function Furniture() {
             #FuniroFurniture
           </p>
         </div>
-        <div className="flex ">
-          <Image
-            src="/images/left-cutted.png"
-            alt="left-cutted"
-            width={50}
-            height={380}
-            quality={100}
-            className="ml-5"
-          />
-          <Image
-            src="/images/laptop.png"
-            alt="laptop"
-            width={450}
-            height={312}
-            quality={100}
-            className="px-4 ml-4 mt-36"
-          />
 
-<Image
-            src="/images/table1.png"
-            alt="table1"
-            width={300}
-            height={242}
-            quality={100}
-            className="ml-8 py-10"
-          />
-       <div className="flex justify-end px-4 ml-2">
-  <div className="relative w-[290px] h-[348px] mt-24">
-    <Image
-      src="/images/bed.png"
-      alt="bed"
-      layout="fill"
-      objectFit="cover" // Ensures the image covers the area and maintains aspect ratio
-      quality={100}
-    />
-  </div>
-  <div className="relative w-[225px] h-[433px] ml-4">
-    <Image
-      src="/images/table2.png"
-      alt="table2"
-      layout="fill"
-      objectFit="cover" // Ensures the image covers the area and maintains aspect ratio
-      quality={100}
-    />
-  </div>
-</div>
-
+        {/* Desktop Layout */}
+        <div className="hidden lg:grid grid-cols-13 grid-rows-3 gap-4 h-[800px]">
+          {galleryImages.map((image) => (
+            <div
+              key={image.id}
+              className={`${image.className} overflow-hidden`}
+            >
+              <Image
+                src={image.src}
+                alt={image.alt}
+                width={image.width}
+                height={image.height}
+                quality={100}
+                className="w-full h-full object-cover rounded-lg hover:scale-105 transition-transform duration-300"
+              />
+            </div>
+          ))}
         </div>
-        <div className="flex mb-4 w-full">
-  {/* Left Aligned Images */}
-  <div className="flex  mr-80 ">
-    <Image
-      src="/images/chair.png"
-      alt="chair"
-      width={135}
-      height={380}
-      quality={100}
-      className="ml-5 py-4"
-    />
-    <Image
-      src="/images/showpiece.png"
-      alt="showpiece"
-      width={380}
-      height={242}
-      quality={100}
-      className="px-4 py-4 mb-16"
-    />
-  </div>
 
-  {/* Right Aligned Images */}
-  <div className="flex  ">
-    <Image
-      src="/images/artwall.png"
-      alt="artwall"
-      width={178}
-      height={242}
-      quality={100}
-      className="ml-5 py-4"
-    />
-    <Image
-      src="/images/kitchen.png"
-      alt="kitchen"
-      width={258}
-      height={196}
-      quality={100}
-      className="px-4 py-4 mb-20"
-    />
-  </div>
-</div>
-
-        
+        {/* Mobile Layout */}
+        <div className="lg:hidden grid grid-cols-1 sm:grid-cols-2 gap-4">
+          {galleryImages.map((image) => (
+            <div
+              key={image.id}
+              className={`${
+                image.id === '5' ? 'sm:col-span-2' : ''
+              } overflow-hidden ${image.className}`}
+            >
+              <Image
+                src={image.src}
+                alt={image.alt}
+                width={image.width}
+                height={image.height}
+                quality={100}
+                className="rounded-lg hover:scale-105 transition-transform duration-300"
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
-  );
+  )
 }
